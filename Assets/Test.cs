4f15d4db0ca388599b1b44435024e2995ffcd952
SAE\lwatson2 <liam_watson2003@hotmail.com>
2017-04-10 16:01:32 +1000

    Initial upload of Scripts and test scene, Created Magnitude that returns a float

---
+++ Assets/Test.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    public GameObject test1;
+    public GameObject test2;
+    float distance;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        distance = wobkmath.Instance.GetMag(test1.transform, test2.transform);
+        Debug.Log(distance);
+	}
+}
---
+++ Assets/wobkmath.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wobkmath : MonoBehaviour {
+
+    //Create the Singleton
+    public static wobkmath Instance;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+
+    //wobkmath 
+    //This is my scripting API used to shorten math required by games
+    #region Vector 3 Math
+    //Magnitude Function, takes 2 transforms and returns a float of distance
+    public float GetMag(Transform obj1, Transform obj2)
+    {
+        Vector3 VectorDifference = obj1.position - obj2.position;
+        float Distance = VectorDifference.sqrMagnitude;
+        return Distance;
+    }
+
+    #endregion
+
+}
